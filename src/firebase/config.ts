cf81ceb388238ad89af7b443eb0c7a0f23e14377
Pankaj Chetri <pankajchetri29@gmail.com>
2026-01-29 16:28:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9887159692-3c03a",
+  "appId": "1:19722145581:web:2a6258a7838648358f4da4",
+  "apiKey": "AIzaSyC4oYh-G0SaAafDbT0KlNptaTts27rrtSI",
+  "authDomain": "studio-9887159692-3c03a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "19722145581"
+};
